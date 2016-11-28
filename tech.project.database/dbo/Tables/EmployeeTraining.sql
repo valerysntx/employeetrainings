49b93bd2d26ee7be048cd886461bad02abc4fb79
valery schepaschenko <valery.sntx@gmail.com>
2016-11-28 04:57:00 +0200

    conventions for entity / config reverse generation

--- tech.project.database/dbo/Tables/EmployeeTraining.sql
+++ tech.project.database/dbo/Tables/EmployeeTraining.sql
@@ -2,7 +2,9 @@
     [Id]         UNIQUEIDENTIFIER NOT NULL,
     [EmployeeId] UNIQUEIDENTIFIER NOT NULL,
     [TrainingId] UNIQUEIDENTIFIER NOT NULL,
-    [AttendDate] DATETIME         NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+    [AttendDate] DATETIME         NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_EmployeeTraining_ToEmployee] FOREIGN KEY ([EmployeeId]) REFERENCES [Employee]([Id]), 
+    CONSTRAINT [FK_EmployeeTraining_ToTraining] FOREIGN KEY ([TrainingId]) REFERENCES [Training]([Id])
 );
 
---
+++ tech.project.tests/EmployeeTrainingContextTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tech.project.tests.conventions;
+using System.Data.Entity;
+
+
+namespace tech.project.tests
+{
+
+  using Model = tech.project.tests.conventions;
+
+  [TestFixture]
+  [Description("Persistence test for convention-build context")]
+  public class EmployeeTrainingContextTests
+  {
+
+    [TestFixtureSetUp]
+    public void Setup()
+    {
+      Database.SetInitializer<conventions.Model>(new DropCreateDatabaseAlways<conventions.Model>());
+    }
+
+    [Test]
+    public void ShouldBeAbleToInitializeAndPersistTheModel()
+    {
+      var model = new conventions.Model();
+
+      Assert.That(model != null);
+
+      var employee = 
+        new conventions.Employee() {
+           Id = Guid.NewGuid(),
+           Name = "Guid",
+           Surname = "Hash",
+          Birthdate = DateTime.Parse("01/01/1999")
+        };
+
+      model.Employees.Add(employee);
+
+      Assert.DoesNotThrow(() =>
+      {
+        model.ChangeTracker.DetectChanges(); model.SaveChanges();
+      });
+
+      Assert.That(model.Employees.Count() > 0);
+
+      model.Dispose();
+    }
+
+  }
+}
